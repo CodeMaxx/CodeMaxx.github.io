7830c6009717d18b9a6480914d67c63d7ef5948d
Akash Trehan <akash.trehan123@gmail.com>
2015-12-10 00:30:32 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$(".info").hide();
+/*	$("#info-home").hide();
+	$("#info-about-me").hide();
+	$("#info-projects").hide();
+	$("#info-contact").hide();
+	*/$(".home").fadeTo(0,0.5);
+	$(".about-me").fadeTo(0,0.5);
+	$(".projects").fadeTo(0,0.5);
+	$(".contact").fadeTo(0,0.5);
+	
+    $(".home").click(function(){
+		$(".home").fadeTo(0,1);
+		$(".about-me").fadeTo(0,0.5);
+		$(".projects").fadeTo(0,0.5);
+		$(".contact").fadeTo(0,0.5);
+		$(".info").hide();
+        $("#info-home").fadeIn("slow");      
+    });
+    
+    $(".about-me").click(function(){
+		$(".home").fadeTo(0,0.5);
+		$(".about-me").fadeTo(0,1);
+		$(".projects").fadeTo(0,0.5);
+		$(".contact").fadeTo(0,0.5);
+		$(".info").hide();
+		$("#info-about-me").fadeIn("slow");
+    });
+    $(".projects").click(function(){
+		$(".home").fadeTo(0,0.5);
+		$(".about-me").fadeTo(0,0.5);
+		$(".projects").fadeTo(0,1);
+		$(".contact").fadeTo(0,0.5);
+		$(".info").hide();
+		$("#info-projects").fadeIn("slow");
+    });
+    $(".contact").click(function(){
+		$(".home").fadeTo(0,0.5);
+		$(".about-me").fadeTo(0,0.5);
+		$(".projects").fadeTo(0,0.5);
+		$(".contact").fadeTo(0,1);
+		$(".info").hide();
+		$("#info-contact").fadeIn("slow");
+    });        
+});
